c8836416ce19cd7b5c902ce249a7b305eddcb08a
Sean Coughlin <seanmc4@illinois.edu>
2020-06-27 16:16:34 -0500

    Add templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Sean C',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 27, 2020'
+    },
+    {
+        'author': 'Sean C',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 27, 2020'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'title': 'Home',
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
