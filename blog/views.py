d9e51e8dd553266504abcd549e9c163186885e08
Sean Coughlin <seanmc4@illinois.edu>
2020-06-28 13:50:08 -0500

    Create models and database for blog data

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Sean C',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'June 27, 2020'
-    },
-    {
-        'author': 'Sean C',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'June 27, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'title': 'Home',
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
