311b6caedb73c3ac34500132bc9f97b0996b2859
Sean Coughlin <seanmc4@illinois.edu>
2020-07-01 15:20:24 -0500

    Add first class view and reverse post order

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
+# from django.http import HttpResponse
 from .models import Post
+
+'''
+Class based views
+'''
+
+class PostListView(ListView):
+    model = Post
+    # expected template name -> <app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] # reverses the ordering so newest posts first
+
+'''
+Function based views
+'''
 
 def home(request):
     context = {
