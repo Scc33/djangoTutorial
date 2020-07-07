634dfd847d395967779abfdf9160dc3b0a16bd15
Sean Coughlin <seanmc4@illinois.edu>
2020-07-06 21:10:09 -0500

    Add my own modification and first feature of finance blog

--- users/models.py
+++ users/models.py
@@ -2,11 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+COLOR_CHOICES = (
+    ('technical','Technical'),
+    ('value', 'Value'),
+    ('growth','Growth'),
+)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
     # date_joined = models.DateTimeField(default=timezone.now)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    type = models.CharField(max_length=9, choices=COLOR_CHOICES, default='value')
 
     def __str__(self):
         return f'{self.user.username} Profile'
